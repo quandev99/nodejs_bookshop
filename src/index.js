ed1e6fe7cf398092e2e81e83e445214d353bba20
quandev99 <nguyenvietquan2605@gmail.com>
2023-03-11 15:18:09 +0700

    commit code blog f8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-// const handlebars = require("express-handlebars");
 const { engine } = require("express-handlebars");
 const morgan = require("morgan");
 const { extname } = require("path");
@@ -10,7 +9,7 @@ const port = 3333;
 app.use(express.static(path.join(__dirname, "public")));
 
 //HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 // Template engine
 app.engine(
   "hbs",
@@ -20,12 +19,14 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
-// console.log("PATH:", path.join(__dirname, "resources/views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/new", (req, res) => {
-  res.render("new");
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
 });
 
 app.listen(port, () => {
